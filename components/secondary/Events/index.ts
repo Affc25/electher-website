d0a303bcb31917686c086243509864952939900d
Affc25 <it@elect-her.org>
2025-04-28 10:46:36 +0100

    Revert "Dev"

--- components/secondary/Events/index.ts
+++ components/secondary/Events/index.ts
@@ -1,4 +1,3 @@
 export { default as RoundtableCard } from "./RoundtableCard"
-export { default as RoundTableEvents } from "./RoundTableEvents"
 export { default as SuggestRoundTable } from "./SuggestRoundTable"
 export { default as RoundTableTablist } from "./RoundTableTablist"
